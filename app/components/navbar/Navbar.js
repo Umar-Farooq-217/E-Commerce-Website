56d0cb5f3f92991fd67ec8de9726bb1c4fe09b30
Umar Farooq <uf29664@gmail.com>
2025-06-09 17:18:33 +0500

    navbar

---
+++ app/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div className="">
+            
+        </div>
+        <div className=""></div>
+      
+    </nav>
+  )
+}
